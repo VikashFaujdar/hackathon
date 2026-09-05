417a1465d6e69ef786b6f61b957993c902f08ea1
VikashFaujdar <vk7571027@gmail.com>
2026-09-05 09:19:23 +0530

    all done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/Login.png"
+import Logo from "../assets/Login.png";
 import { Link } from "react-router-dom";
 
 const Login = () => {
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import signup from "../assets/Signup.png";
+import signup from "../assets/signup.png";
 import { Link } from "react-router-dom";  
 
 const Signup = () => {
